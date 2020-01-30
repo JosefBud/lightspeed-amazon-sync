fda77277a9183a395599138a06b5147115d247fc
JosefBud <josefbud@gmail.com>
2020-01-29 21:17:22 -0500

    Test for token refresh function

---
+++ test/functions/lightspeed/refreshToken.test.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const assert = require('assert');
+const decache = require('decache');
+
+/* let accessToken;
+beforeEach(async () => {
+  accessToken = await refreshToken();
+}); */
+
+describe("Lightspeed's refresh token function", () => {
+  it('Returns a token string', async () => {
+    const refreshToken = require('../../../lib/functions/lightspeed/refreshToken.js');
+    const accessToken = await refreshToken();
+    assert.equal(typeof accessToken, 'string');
+    assert.equal(accessToken.length, 40);
+    console.log(accessToken);
+  });
+
+  it('Handles errors', async () => {
+    decache('../../../lib/functions/lightspeed/refreshToken.js');
+    delete process.env.LIGHTSPEED_ID;
+    delete process.env.LIGHTSPEED_SECRET;
+    delete process.env.LIGHTSPEED_REFRESH_TOKEN;
+    const badRefreshToken = require('../../../lib/functions/lightspeed/refreshToken.js');
+    const error = await badRefreshToken();
+    assert.equal(error.isAxiosError, true);
+  });
+});
