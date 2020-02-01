977de66ae6c8be2d1f370210c63348a56c0244a2
JosefBud <josefbud@gmail.com>
2020-02-01 18:01:44 -0500

    Updated main file with some mock vars

--- start.js
+++ start.js
@@ -7,24 +7,70 @@ const getOrderIDs = require('./lib/functions/amazon/getOrderIDs.js');
 const getOrderItems = require('./lib/functions/amazon/getOrderItems.js');
 const buildInventoryFeed = require('./lib/functions/amazon/buildInventoryFeed.js');
 const submitInventoryFeed = require('./lib/functions/amazon/submitInventoryFeed.js');
+const getItemIDs = require('./lib/functions/lightspeed/getItemIDs.js');
+const createInventoryCount = require('./lib/functions/lightspeed/createInventoryCount.js');
+const reconcileInventoryCount = require('./lib/functions/lightspeed/reconcileInventoryCount.js');
 const lightspeedApi = 'https://api.lightspeedapp.com/API';
 
 (async () => {
-  // get a list of order IDs - the time range is measured in minutes ** getOrderIDs(minutes) **
-  // the end of the time range is not now but 60 minutes in the past, due to an issue with the MWS API
-  const orders = await getOrderIDs(15);
-  console.log(orders);
-  if (orders === undefined || orders.length === 0) {
-    return;
-  }
-
-  // get a list of SKUs and their quantities ordered using the order IDs
-  const orderItems = await getOrderItems(orders);
-  console.log('complete', orderItems);
-
-  return;
   // refresh the token
   const accessToken = await refreshToken();
+  if (typeof accessToken == 'string') {
+    // creating the authentication header for all future API calls
+    const authHeader = {
+      Authorization: `Bearer ${accessToken}`
+    };
+
+    // getting the account ID
+    const accountID = await getAccountID(authHeader);
+
+    // get a list of order IDs - the time range is measured in minutes ** getOrderIDs(minutes) **
+    // the end of the time range is not now but 60 minutes in the past, due to an issue with the MWS API
+    const orders = await getOrderIDs(240);
+    if (orders === undefined || orders.length === 0) {
+      return;
+    }
+
+    // get a list of SKUs and their quantities ordered using the order IDs
+    let orderItems = await getOrderItems(orders);
+    const mockOrderItems = [
+      {
+        itemSKU: '22789',
+        qty: 1
+      },
+      {
+        itemSKU: '22790',
+        qty: 2
+      },
+      {
+        itemSKU: '22791',
+        qty: 3
+      },
+      {
+        itemSKU: '22792',
+        qty: 1
+      }
+    ];
+
+    // get Lightspeed item IDs and current sellable quantities of the order items
+    orderItems = await getItemIDs(authHeader, accountID, mockOrderItems);
+
+    // creates an inventory count and fills it with the order items
+    const inventoryCountID = await createInventoryCount(
+      authHeader,
+      accountID,
+      orderItems
+    );
+
+    // reconciles the inventory count, finalizing the sync
+    // on a 2-second timeout for rate limiting
+    setTimeout(async () => {
+      await reconcileInventoryCount(authHeader, accountID, inventoryCountID);
+    }, 2000);
+  }
+  return;
+  // refresh the token
+  accessToken = await refreshToken();
   if (typeof accessToken == 'string') {
     // creating the authentication header for all future API calls
     const authHeader = {
