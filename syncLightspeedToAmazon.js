68c7eea9a34ea53284424798cd0cfa8ac1fa6a68
JosefBud <josefbud@gmail.com>
2020-02-13 21:10:16 -0500

    Adding loggers and catch statements

--- syncLightspeedToAmazon.js
+++ syncLightspeedToAmazon.js
@@ -5,11 +5,12 @@ const parseSales = require('./lib/functions/lightspeed/parseSales.js');
 const getSKUs = require('./lib/functions/lightspeed/getSKUs.js');
 const buildInventoryFeed = require('./lib/functions/amazon/buildInventoryFeed.js');
 const submitInventoryFeed = require('./lib/functions/amazon/submitInventoryFeed.js');
+const logger = require('./lib/logger.js');
 
 const syncLightspeedToAmazon = async () => {
   return new Promise(async (resolve, reject) => {
     // refresh the token
-    accessToken = await refreshToken();
+    accessToken = await refreshToken().catch(err => console.error(err));
     if (typeof accessToken == 'string') {
       // creating the authentication header for all future API calls
       const authHeader = {
@@ -17,19 +18,31 @@ const syncLightspeedToAmazon = async () => {
       };
 
       // getting the account ID
-      const accountID = await getAccountID(authHeader);
+      const accountID = await getAccountID(authHeader).catch(err =>
+        console.error(err)
+      );
 
       // getting the past 60 minutes of Lightspeed sales
-      const salesRaw = await getSales(authHeader, accountID, 60);
+      const salesRaw = await getSales(
+        authHeader,
+        accountID,
+        60 * 24 * 30
+      ).catch(err => console.error(err));
       if (salesRaw['@attributes'] && salesRaw['@attributes'].count == '0') {
+        logger.log({
+          level: 'info',
+          message:
+            'No sales were found, so the Lightspeed to Amazon sync is ending.'
+        });
         return;
       }
-
       // parsing the sales into an array of small objects that only contain necessary info
-      let sales = await parseSales(salesRaw);
+      let sales = await parseSales(salesRaw).catch(err => console.error(err));
 
       // querying the API to get the SKU and sellable quantity for each sale item
-      let inventory = await getSKUs(authHeader, accountID, sales);
+      let inventory = await getSKUs(authHeader, accountID, sales).catch(err =>
+        console.error(err)
+      );
 
       // pruning items that don't have SKUs
       inventory = inventory.filter(item => item.SKU.length > 0);
@@ -37,10 +50,12 @@ const syncLightspeedToAmazon = async () => {
       // builds an XML file for submitting to Amazon as an inventory update
       // the second function parameter is for your "fulfillment latency" on Amazon; the time it
       // takes you to ship an item.. unfortunately this is a required field on Amazon's end.
-      const inventoryFeed = await buildInventoryFeed(inventory, 4);
+      const inventoryFeed = await buildInventoryFeed(inventory, 4).catch(err =>
+        console.error(err)
+      );
 
       // submits the inventory feed to Amazon
-      await submitInventoryFeed(inventoryFeed);
+      await submitInventoryFeed(inventoryFeed).catch(err => console.error(err));
       resolve();
     } else {
       reject();
