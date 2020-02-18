be4e9ba9ec0be950850c2eaee13e4dff20757c39
JosefBud <josefbud@gmail.com>
2020-02-18 14:51:31 -0500

    New branch using a full sync instead of only sales

--- start.js
+++ start.js
@@ -1,13 +1,16 @@
 const syncAmazonToLightspeed = require('./syncAmazonToLightspeed.js');
 const syncLightspeedToAmazon = require('./syncLightspeedToAmazon.js');
+const massSync = require('./massSync.js');
 
 (async () => {
   await syncAmazonToLightspeed();
-  await syncLightspeedToAmazon();
+  //await syncLightspeedToAmazon();
+  await massSync();
 })();
 
 // 15-minute interval
 setInterval(async () => {
   await syncAmazonToLightspeed();
-  await syncLightspeedToAmazon();
+  //await syncLightspeedToAmazon();
+  await massSync();
 }, 900000);
