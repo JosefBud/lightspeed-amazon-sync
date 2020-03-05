1d21ed080f94b75978d6aba3d156a3ffe88ea253
JosefBud <josefbud@gmail.com>
2020-03-05 15:12:46 -0500

    Moving printer function calls to main script

--- start.js
+++ start.js
@@ -2,6 +2,10 @@ const syncAmazonToLightspeed = require('./syncAmazonToLightspeed.js');
 const syncLightspeedToAmazon = require('./syncLightspeedToAmazon.js');
 const massSync = require('./massSync.js');
 
+const orderGrabber = require('./lib/functions/printer/orderGrabber.js');
+const orderItemGrabber = require('./lib/functions/printer/orderItemGrabber.js');
+const invoiceCreator = require('./lib/functions/printer/invoiceCreator.js');
+
 (async () => {
   const thereAreOrders = await syncAmazonToLightspeed();
   const thereAreSales = await syncLightspeedToAmazon();
@@ -9,6 +13,15 @@ const massSync = require('./massSync.js');
   if (thereAreOrders || thereAreSales) {
     await massSync();
   }
+
+  let ordersToPrint = await orderGrabber();
+  if (ordersToPrint.length > 0) {
+    ordersToPrint = await orderItemGrabber(ordersToPrint);
+    ordersToPrint.forEach(order => {
+      await invoiceCreator(order);
+    });
+  }
+
   setTimeout(() => {
     process.exit(1);
   }, 2500);
